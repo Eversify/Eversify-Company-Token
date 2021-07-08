27c496d641f561588a416c972ed59ae5f62b3c50
Eversify <87110988+Eversify@users.noreply.github.com>
2021-07-08 01:17:44 -0400

    Add files via upload

---
+++ Eversify.sol
@@ -0,0 +1,388 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.4;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        return c;
+    }
+}
+
+contract Ownable is Context {
+    address private _owner;
+    address private _previousOwner;
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    constructor() {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+}
+
+interface IUniswapV2Factory {
+    function createPair(address tokenA, address tokenB)
+        external
+        returns (address pair);
+}
+
+interface IUniswapV2Router02 {
+    function swapExactTokensForETHSupportingFeeOnTransferTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external;
+
+    function factory() external pure returns (address);
+
+    function WETH() external pure returns (address);
+
+    function addLiquidityETH(
+        address token,
+        uint256 amountTokenDesired,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        payable
+        returns (
+            uint256 amountToken,
+            uint256 amountETH,
+            uint256 liquidity
+        );
+}
+
+contract Eversify is Context, IERC20, Ownable {
+    using SafeMath for uint256;
+
+    string private constant _name = "Eversify Token";
+    string private constant _symbol = "EVE";
+    uint8 private constant _decimals = 8;
+
+    // Total supply of Token
+    uint256 private _tTotal = 10*10**9 * 10**8;
+    // Tax fees in %
+    uint256 private _taxFees = 3;
+    address payable private _taxAddress;
+
+    uint32 public TradeLimitTime = uint32(block.timestamp);
+
+    // Keeps track of Balance of a particilar addresses
+    //eg:  _tOwned[address] == 100;
+    mapping(address => uint256) private _tOwned;
+    mapping(address => bool) private _isExcludedFromFee;
+
+    // Shows the allowance of a user from another user
+    // eg:  _allowances[owner][spender] == 100 ;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    IUniswapV2Router02 private uniswapV2Router;
+    address private uniswapV2Pair;
+    bool private tradingOpen;
+    bool private inSwap = false;
+    bool private swapEnabled = false;
+
+    uint256 private _maxTxAmount = _tTotal;
+
+    event MaxTxAmountUpdated(uint256 _maxTxAmount);
+
+    modifier lockTheSwap {
+        inSwap = true;
+        _;
+        inSwap = false;
+    }
+
+    // This function is called only onces when the contract is deployed,
+    // It takes one input parameter which is the address for the wallet which
+    // will receive the taxFees
+    constructor(address payable taxAddress) {
+        _taxAddress = taxAddress;
+        _tOwned[_msgSender()] = _tTotal;
+        _isExcludedFromFee[owner()] = true;
+        _isExcludedFromFee[address(this)] = true;
+        _isExcludedFromFee[_taxAddress] = true;
+        emit Transfer(address(0), _msgSender(), _tTotal);
+    }
+
+    // Read(View) function
+    function name() public pure returns (string memory) {
+        return _name;
+    }
+
+    // Read(View) function
+    function symbol() public pure returns (string memory) {
+        return _symbol;
+    }
+
+    // Read(View) function
+    function decimals() public pure returns (uint8) {
+        return _decimals;  // Returns "8"
+    }
+
+    // Read(View) function
+    function totalSupply() public view override returns (uint256) {
+        return _tTotal; // Returns 1000000000000 ==> (10 Billion) + decimals(8)
+    }
+
+    // Read(View) function
+    function balanceOf(address account) public view override returns (uint256) {
+        return _tOwned[account]; // returns token balance of any address
+    }
+
+    // Basic token-transfer function
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        // Calls Upgraded transfer function
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
+        return true;
+    }
+
+    function removeAllFee() private {
+        if (_taxFees == 0) return;
+        _taxFees = 0;
+    }
+
+    function restoreAllFee() private {
+        _taxFees = 3;
+    }
+
+    function _approve(address owner, address spender, uint256 amount) private {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _transfer(address from, address to, uint256 amount) private {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(amount > 0, "Transfer amount must be greater than zero");
+
+        if (from != owner() && to != owner() && to != uniswapV2Pair && to != address(uniswapV2Router)) {
+            if(uint256(TradeLimitTime) >= block.timestamp){
+                require(balanceOf(to).add(amount) <= _maxTxAmount," Please decrease transaction amount or wait...");
+            }
+
+            uint256 contractTokenBalance = balanceOf(address(this));
+
+            if (!inSwap && from != uniswapV2Pair && swapEnabled ) {
+                swapTokensForEth(contractTokenBalance);
+                uint256 contractETHBalance = address(this).balance;
+                if (contractETHBalance > 0) {
+                    sendETHToFee(address(this).balance);
+                }
+            }
+        }
+
+        bool takeFee = true;
+        if (_isExcludedFromFee[from] || _isExcludedFromFee[to]) {
+            takeFee = false;
+        }
+
+        _tokenTransfer(from, to, amount, takeFee);
+    }
+
+    function swapTokensForEth(uint256 tokenAmount) private lockTheSwap {
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = uniswapV2Router.WETH();
+        _approve(address(this), address(uniswapV2Router), tokenAmount);
+        uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            tokenAmount,
+            0,
+            path,
+            address(this),
+            block.timestamp
+        );
+    }
+
+    function sendETHToFee(uint256 amount) private {
+        _taxAddress.transfer(amount);
+    }
+
+    function openTrading() external onlyOwner() {
+        require(!tradingOpen, "trading is already open");
+        // declare uniswap router
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+
+        // assign router address
+        uniswapV2Router = _uniswapV2Router;
+
+        // set initial time limit
+        TradeLimitTime = uint32((block.timestamp).add(10 minutes)); // XX
+
+        // approve Router for contract
+        _approve(address(this), address(uniswapV2Router), _tTotal);
+
+        // create TOKEN-ETH pair
+        uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
+            .createPair(address(this), _uniswapV2Router.WETH());
+
+        // add liquidity to the pair
+        uniswapV2Router.addLiquidityETH{value: address(this).balance}(
+            address(this),
+            balanceOf(address(this)),
+            0,
+            0,
+            owner(),
+            block.timestamp
+        );
+
+        // Set swapEnabled to true
+        swapEnabled = true;
+
+        // Set transaction Limit
+        _maxTxAmount = 10*10**7 * 10**8;  // Update to 1% of totalSupply
+
+        // mark Trading as open - to prevent calling this function again
+        tradingOpen = true;
+
+        // approve Router for Uniswap Pair
+        IERC20(uniswapV2Pair).approve(
+            address(uniswapV2Router),
+            type(uint256).max
+        );
+    }
+
+    function _tokenTransfer(address sender, address recipient, uint256 amount, bool takeFee) private {
+        if (!takeFee) removeAllFee();
+        _transferStandard(sender, recipient, amount);
+        if (!takeFee) restoreAllFee();
+    }
+
+    function _transferStandard(address sender, address recipient, uint256 tAmount) private {
+        uint256 tTax = tAmount.mul(_taxFees).div(100);
+        uint256 tTransferAmount= tAmount.sub(tTax);
+        _tOwned[sender] = _tOwned[sender].sub(tAmount);
+        _tOwned[recipient] = _tOwned[recipient].add(tTransferAmount);
+        _takeTax(tTax);
+        emit Transfer(sender, recipient, tTransferAmount);
+    }
+
+    // add tax to contract
+    function _takeTax(uint256 tTax) private {
+        _tOwned[address(this)] = _tOwned[address(this)].add(tTax);
+    }
+
+    receive() external payable {}
+
+}
