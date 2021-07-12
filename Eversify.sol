31380dfb9b86c16748ae6ab1a448ec1e165f81a1
Eversify <87110988+Eversify@users.noreply.github.com>
2021-07-12 11:07:01 -0400

    Update Eversify.sol

--- Eversify.sol
+++ Eversify.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicensed
-pragma solidity ^0.8.4;
+pragma solidity 0.8.4;
 
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) {
